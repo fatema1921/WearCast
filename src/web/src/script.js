ae4f31c56f2bcc040456a1feda16bc24c02569d8
obymael <82178035+obymael@users.noreply.github.com>
2024-05-07 06:09:46 +0200

    Implement initial web interface for WearCast project
    This setup serves as a starting point for further interactive and style enhancements to the WearCast web application. Major changes include:
    
    - New  featuring a initial layout setup with header, navigation, weather information, style recommendations, contact section, and footer
    - Integrate new CSS stylings in  to define and enhance the visual aspects
    - Add basic interaction logic in , including day and location fetching

--- src/web/src/script.js
+++ src/web/src/script.js
@@ -1,5 +1,38 @@
+window.onload=function() {
+    updateCurrentDay();
+    fetchLocation();
+}
+
+function updateCurrentDay() {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date();
+    const currentDay = days[date.getDay()];
+
+    document.getElementById('weekday').textContent = currentDay;
+}
+
+function fetchLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            /* EDIT FOR LATER: API KEY SHOULD BE SECURE NOT PUT HERE! EITHER SERVER SIDE WITH ENVIRONMENT VARIABLES OR IN GITIGNORE? */
+            fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=3661a45eca484dfdb9eda5299d447535`)
+                .then(response => response.json())
+                .then(data => {
+                    const city = data.results[0].components.city;
+                    document.getElementById('location').textContent = city;
+                })
+                .catch(error => console.log('Error fetching location:', error));
+        });
+    } else {
+        document.getElementById('location').textContent = 'Geolocation is not supported by this browser.';
+    }
+}
+
 // Create an MQTT client instance
-var client = new Paho.MQTT.Client("test.mosquitto.org", 1883, "clientId");
+var client = new Paho.MQTT.Client("broker.emqx.io", 8083, "clientId");
+// var client = new Paho.MQTT.Client("test.mosquitto.org", 1883, "clientId");
 
 // Set callback handlers
 client.onConnectionLost = onConnectionLost;
@@ -16,6 +49,7 @@ function onConnect() {
     console.log("Connected to MQTT broker");
     // Subscribe to a topic
     client.subscribe("Temperature");
+    client.subscribe("Humidity");
 }
 
 // Called when the client loses its connection
@@ -28,6 +62,20 @@ function onConnectionLost(responseObject) {
 // Called when a message arrives
 function onMessageArrived(message) {
     console.log("Message received: " + message.payloadString);
+    // Handle the incoming messages
+    // Extract temperature and humidity value based on topic
+    var topic = message.destinationName;
+    var sensorValue = message.payloadString;
+    if (topic === "Temperature") {
+        document.getElementById("temperatureParagraph").textContent = "Temperature: " + sensorValue + " C";
+    } else if (topic === "Humidity") {
+        document.getElementById("humidityParagraph").textContent = "Humidity: " + sensorValue + " % RH";
+    }
+}
+/*
+// Called when a message arrives
+function onMessageArrived(message) {
+    console.log("Message received: " + message.payloadString);
 
     // Handle the incoming message
     // Extract temperature value from the message payload
@@ -35,3 +83,4 @@ function onMessageArrived(message) {
     document.getElementById("temperatureParagraph").textContent = "Temperature: " + temperatureValue;
 
 }
+ */
