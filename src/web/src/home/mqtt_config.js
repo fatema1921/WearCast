9c1fda9ea10f74c145ab863510563d3109540e88
rawanah <rawanah@student.chalmers.se>
2024-05-19 14:02:28 +0200

    #21 Delete secure files

--- src/web/src/home/mqtt_config.js
+++
@@ -1,3 +0,0 @@
-export const TEMPERATURE_TOPIC = "/WearCast/sensor/temperature"; // Applicaiton subscribes to this topic to recieve temprature
-export const HUMIDITY_TOPIC = "/WearCast/sensor/humidity"; // Application subscribes to this topic to recieve humidity
-export const SERVER = "broker.emqx.io"; // Application connects to this MQTT broker
